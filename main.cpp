afb61a77c071d560f69bcd41c1971034f49d3fbf
AsdoubleU <ksw990106@gmail.com>
2023-12-04 01:23:53 +0900

    Swing up failed

--- main.cpp
+++ main.cpp
@@ -6,11 +6,23 @@
 #include "Eigen/Dense.h"
 #include "Parameters.hpp"
 
+// #define Kp 214.3207
+// #define Kd -274.6154
+// #define Kp 1000
+// #define Kd -100
+// #define Kp 700
+// #define Kd -1000
+#define Kp 300
+#define Kd -10
+
+
 DigitalOut led1(LED1);
 Timer measure_time;
 QEncoder  enco(D8,D9);
 double theta, pre_theta, theta_dot, u;
 Eigen::Vector2d state;
+
+bool flag;
 
 int main(void)
 {
@@ -31,7 +43,7 @@ int main(void)
 
     motor->set_speed(0,100);
     motor->run(0, BDCMotor::FWD);
-    wait(0.5);
+    wait(1);
     motor->hard_hiz(1);
     motor->hard_hiz(0);
 
@@ -40,21 +52,26 @@ int main(void)
         measure_time.reset();
         measure_time.start();
 
-        theta = (0.8)*enco.getCount()*(2*PI/8200) + (0.2)*pre_theta;
-
-        measure_time.stop();
-
-        theta_dot = (theta - pre_theta)/measure_time.read();
-        pre_theta = theta;
-        state << theta,theta_dot;
-
-        double energy = (1/2)*Jp*(theta_dot)*(theta_dot) + M*G*lp*(1-cos(theta));
-
-        u = (10)*(energy - E_ref)*theta_dot;
+        if((theta < 3.3 && theta > 3.0) || (theta > -3.3 && theta < -3.0)){
+            if(theta < 0) {theta = 2*PI + theta;}
+            u = Kp*(theta - PI) - Kd*theta_dot;
+        }
+        else{
+            double energy = (1/2)*Jp*(theta_dot)*(theta_dot) + M*G*lp*(1-cos(theta));
+            u = (10)*(energy - E_ref)*theta_dot;
+        }
 
         motor_control(u);
 
         std::cout<<"theta : "<<theta<<" , theta_dot : "<<theta_dot<<std::endl;
+
+        measure_time.stop();
+
+        // Update states
+        theta = (0.8)*enco.getCount()*(2*PI/8200) + (0.2)*pre_theta;
+        theta_dot = (theta - pre_theta)/measure_time.read();
+        pre_theta = theta;
+        state << theta,theta_dot;
 
         }
 }
